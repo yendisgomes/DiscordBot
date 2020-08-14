c14008c358673f4a91613dee79795cd82add2419
Yendis Gomes <yendis.gomes@hotmail.com>
2020-08-14 00:44:13 -0300

    Discord Bot in C#

---
+++ DiscordBot/Modules/Commands.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+        }
+    }
+}
